5db23bbdaebad88d0c0171889947021b8a0b016a
Moaz Hamza <moaz.hamza@kensho.com>
2024-12-05 15:06:14 -0500

    Day 2 Part 2

--- aoc_2024/day_2/day_2.py
+++ aoc_2024/day_2/day_2.py
@@ -32,12 +32,64 @@ So, in this example, 2 reports are safe.
 
 Analyze the unusual data from the engineers. How many reports are safe?
 
+--- Part Two ---
+The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
 
+The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+More of the above example's reports are now safe:
+
+7 6 4 2 1: Safe without removing any level.
+1 2 7 8 9: Unsafe regardless of which level is removed.
+9 7 6 2 1: Unsafe regardless of which level is removed.
+1 3 2 4 5: Safe by removing the second level, 3.
+8 6 4 4 1: Safe by removing the third level, 4.
+1 3 6 7 9: Safe without removing any level.
+Thanks to the Problem Dampener, 4 reports are actually safe!
+
+Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
 """
 
+import dataclasses
+from enum import StrEnum
 from typing import List
 
 from aoc_2024.utils import load_input
+
+
+class SafetyReason(StrEnum):
+    UNIFORMITY = "uniform"
+    LARGE_DIFF = "difference"
+
+
+@dataclasses.dataclass
+class SafetyResult:
+    safe: bool
+    ascending: bool = True
+
+    bad_index: int = -1
+    reason: SafetyReason | None = None
+
+    def __bool__(self) -> bool:
+        return self.safe
+
+
+def _is_safe(level_list: List[int]) -> SafetyResult:
+    ascending = True if level_list[0] < level_list[1] else False
+
+    if not ascending:
+        level_list = list(reversed(level_list))
+
+    for i in range(0, len(level_list) - 1):
+        if level_list[i] >= level_list[i + 1]:
+            return SafetyResult(False, ascending, i, SafetyReason.UNIFORMITY)
+        diff = level_list[i + 1] - level_list[i]
+        if diff > 3 or diff < 1:
+            return SafetyResult(False, ascending, i, SafetyReason.LARGE_DIFF)
+
+    return SafetyResult(True)
 
 
 def part_1(input_list: List[str]) -> int:
@@ -65,10 +117,58 @@ def part_1(input_list: List[str]) -> int:
     return num_safe_reports
 
 
+def part_1_updated(input_list: List[str]) -> int:
+    num_safe_reports = 0
+    for report in input_list:
+        report_list = [int(level) for level in report.split(" ")]
+
+        report_is_safe = _is_safe(report_list)
+        if report_is_safe:
+            num_safe_reports += 1
+    return num_safe_reports
+
+
+def _dampen_list(report_list: List[int], bad_index: int) -> bool:
+    new_list = list(report_list)
+    # If the report is not safe, try to remove one of the problem levels
+    new_list.pop(bad_index)
+
+    return _is_safe(new_list).safe
+
+
+def _is_safe_pt_2(level_list: List[int]) -> bool:
+    original_report_is_safe = _is_safe(level_list)
+    safe = original_report_is_safe.safe
+    if not safe:
+        for problem_index in range(len(level_list)):
+            # Try removing that problem index
+            safe = _dampen_list(level_list, problem_index)
+            if safe:
+                return True
+    else:
+        return True
+    return False
+
+
+def part_2(input_list: List[str]) -> int:
+    num_safe_reports = 0
+    for report in input_list:
+        report_list = [int(level) for level in report.split(" ")]
+        if _is_safe_pt_2(report_list):
+            num_safe_reports += 1
+
+    return num_safe_reports
+
+
 def main():
     input_list = load_input()
     part_1_answer = part_1(input_list)
     print(f"Part 1 result: {part_1_answer}")
+    part_1_updated_answer = part_1_updated(input_list)
+    print(f"Part 1 updated result: {part_1_updated_answer}")
+
+    part_2_answer = part_2(input_list)
+    print(f"Part 2 result: {part_2_answer}")
 
 
 if __name__ == "__main__":
